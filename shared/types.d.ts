b645d592ed1f06f4a28b029fccfb4ec818e60587
Emma <emmanolan2019@gmail.com>
2023-11-23 22:30:41 +0000

    Added Movie review type

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -11,3 +11,13 @@ export type Movie = {
     vote_average: number;
     vote_count: number
   }
+
+  export type MovieReview = {
+    reviewId: number;
+    movieId: number;
+    userId: number;
+    rating: number;
+    comment: string;
+    timestamp: string; // or use a Date type
+  };
+  
